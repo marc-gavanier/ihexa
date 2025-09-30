dd5fb5640ee1c18c3e9cc1f61b9a271eb7c90cc8
Marc Gavanier <marc.gavanier@protonmail.com>
2025-09-30 21:04:29 +0200

    feat: invoice domain model

---
+++ src/features/invoice/domain/invoice.ts
@@ -0,0 +1,45 @@
+import type { DomainModel } from '@/libraries/ddd/domain-model';
+
+type InvoiceId = DomainModel<'InvoiceId', string>;
+
+type Name = DomainModel<
+  'Name',
+  {
+    firstName: string;
+    lastName: string;
+  }
+>;
+
+type Street = DomainModel<'Street', string>;
+
+type City = DomainModel<'City', string>;
+
+type PostalCode = DomainModel<'PostalCode', string>;
+
+type Address = DomainModel<
+  'Address',
+  { street: Street; city: City; postalCode: PostalCode }
+>;
+
+type Recipient = DomainModel<'Recipient', { name: Name; address: Address }>;
+
+type LineLabel = DomainModel<'LineLabel', string>;
+
+type Amount = DomainModel<'Amount', number>;
+
+type Quantity = DomainModel<'Quantity', number>;
+
+type Line = DomainModel<
+  'Line',
+  { label: LineLabel; unitPrice: Amount; quantity: Quantity }
+>;
+
+export type Invoice = DomainModel<
+  'Invoice',
+  {
+    id: InvoiceId;
+    recipient: Recipient;
+    lines: Line[];
+    total: Amount;
+  }
+>;
