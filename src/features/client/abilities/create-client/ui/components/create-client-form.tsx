49f1412e20875f78477168e70a194b243de3726f
Marc Gavanier <marc@gavanier.com>
2026-03-28 01:34:15 +0100

    feat: reset form after client creation instead of redirect
    - Replace redirect to home with form.reset() on success
    - Use useRef to access form.reset in useServerAction callback
    - Update e2e test to verify form is cleared after submission

--- src/features/client/abilities/create-client/ui/components/create-client-form.tsx
+++ src/features/client/abilities/create-client/ui/components/create-client-form.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import { applyEffectSchema } from '@/libraries/form/apply-effect-schema';
+import { fieldErrorTranslation } from '@/libraries/form/field-error-translation';
 import { handleAction } from '@/libraries/form/handle-action';
 import { handleSubmit } from '@/libraries/form/handle-submit';
 import { useAppForm } from '@/libraries/form/use-app-form';
@@ -13,9 +14,9 @@ import { CREATE_CLIENT_ACTION_KEY } from '../../injection';
 
 export const CreateClientForm = () => {
   const { t } = useTranslation('clients.create');
-  const router = useRouter();
+  const resetFormRef = useRef<() => void>(() => {});
   const [action, isPending] = useServerAction(inject(CREATE_CLIENT_ACTION_KEY), {
-    onSuccess: () => router.push('/')
+    onSuccess: () => resetFormRef.current()
   });
 
   const form = useAppForm({
@@ -30,6 +31,7 @@ export const CreateClientForm = () => {
     validators: { onSubmit: applyEffectSchema(createClientValidation) },
     onSubmit: handleAction(action)
   });
+  resetFormRef.current = () => form.reset();
 
   return (
     <form.AppForm>
@@ -39,7 +41,7 @@ export const CreateClientForm = () => {
             <div>
               <field.Label>{t('form.firstname.label')}</field.Label>
               <field.Input isPending={isPending} />
-              <field.Info formatMessage={t} />
+              <field.Error formatMessage={t} template={fieldErrorTranslation} />
             </div>
           )}
         </form.AppField>
@@ -48,7 +50,7 @@ export const CreateClientForm = () => {
             <div>
               <field.Label>{t('form.lastname.label')}</field.Label>
               <field.Input isPending={isPending} />
-              <field.Info formatMessage={t} />
+              <field.Error formatMessage={t} template={fieldErrorTranslation} />
             </div>
           )}
         </form.AppField>
@@ -57,7 +59,7 @@ export const CreateClientForm = () => {
             <div>
               <field.Label>{t('form.street.label')}</field.Label>
               <field.Input isPending={isPending} />
-              <field.Info formatMessage={t} />
+              <field.Error formatMessage={t} template={fieldErrorTranslation} />
             </div>
           )}
         </form.AppField>
@@ -66,7 +68,7 @@ export const CreateClientForm = () => {
             <div>
               <field.Label>{t('form.city.label')}</field.Label>
               <field.Input isPending={isPending} />
-              <field.Info formatMessage={t} />
+              <field.Error formatMessage={t} template={fieldErrorTranslation} />
             </div>
           )}
         </form.AppField>
@@ -75,7 +77,7 @@ export const CreateClientForm = () => {
             <div>
               <field.Label>{t('form.zipcode.label')}</field.Label>
               <field.Input isPending={isPending} />
-              <field.Info formatMessage={t} />
+              <field.Error formatMessage={t} template={fieldErrorTranslation} />
             </div>
           )}
         </form.AppField>
