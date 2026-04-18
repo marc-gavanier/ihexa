9a2c6d1cfb8a429955af7d5797e8187e0a5a8f6e
Marc Gavanier <marc@gavanier.com>
2026-04-18 10:50:29 +0200

    feat: improve form library with QueryLink and ErrorMessage enhancements
    - Add QueryLink form component for linking with form values as query params
    - Improve ErrorMessage: use hasError() for error detection, simplify formatMessage default
    - Register QueryLink in useAppForm form components

---
+++ src/libraries/form/form/query-link.form.tsx
@@ -0,0 +1,33 @@
+import type { ReactFormExtendedApi } from '@tanstack/react-form';
+import { Link, type LinkProps } from '@/libraries/ui/primitives/link';
+import { useFormContext } from '../form-context';
+
+export const QueryLink = ({
+  queryParam,
+  pathname,
+  ...props
+}: {
+  queryParam: string;
+  pathname: string;
+} & Omit<LinkProps, 'href'>) => {
+  const form = useFormContext() as unknown as ReactFormExtendedApi<
+    Record<string, string>,
+    never,
+    never,
+    never,
+    never,
+    never,
+    never,
+    never,
+    never,
+    never,
+    never,
+    never
+  >;
+
+  return (
+    <form.Subscribe selector={({ values }) => values[queryParam]}>
+      {(value) => <Link href={{ pathname, query: (value?.length ?? 0) > 0 ? { [queryParam]: value } : {} }} {...props} />}
+    </form.Subscribe>
+  );
+};
