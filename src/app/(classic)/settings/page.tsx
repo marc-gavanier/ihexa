ed48f1e01dcb0eaffe6be2c1f53f224afe0f9a65
Marc Gavanier <marc@gavanier.com>
2026-05-05 19:19:00 +0200

    feat: add congigure seller settings page

---
+++ src/app/(classic)/settings/page.tsx
@@ -0,0 +1,23 @@
+import { withOptionalEither } from '@arckit/nextjs';
+import { configureSellerAction } from '@/app/_actions/settings/configure-seller.action';
+import { searchCompanyAction } from '@/app/_actions/settings/search-company.action';
+import { i18n, type MetadataTranslation, metadataTranslation, withI18n } from '@/configuration/i18n';
+import { pageBuilder, withClientBinder } from '@/configuration/nextjs';
+import {
+  CONFIGURE_SELLER_KEY,
+  ConfigureSellerPage,
+  presentSeller,
+  SEARCH_COMPANY_KEY
+} from '@/features/settings/abilities/configure-seller';
+import { getSellerConfiguration } from '@/features/settings/abilities/configure-seller/implementations';
+
+export const generateMetadata: MetadataTranslation = metadataTranslation(i18n)('settings.configure-seller');
+
+export default pageBuilder()
+  .use(
+    withClientBinder(CONFIGURE_SELLER_KEY, configureSellerAction),
+    withClientBinder(SEARCH_COMPANY_KEY, searchCompanyAction),
+    withI18n(i18n)('settings.configure-seller', 'global.server-action')
+  )
+  .use(withOptionalEither('seller', () => getSellerConfiguration()))
+  .render(async ({ seller }) => <ConfigureSellerPage seller={presentSeller(seller)} />);
